11e604480d7c2f6a926b91254af81bf9040626c9
Micho <github@michael-kreil.de>
2012-12-13 22:19:19 +0100

    add comments

--- start.node.js
+++ start.node.js
@@ -1,9 +1,17 @@
-// Run this to receive a raw image stream from your drone.
+// Starts the drone.
+// Receives video of the front camera as uncompressed frames
+//
+// Marker is a simple radial gradient from green to black.
+// Unfortunately the front camera is not that good so the bright green in the center will become yellow and white
+// Since we are looking for green pixels, the gradient will become a circle.
+// Find this circle and keep the position in front of the marker
 
 var arDrone = require('ar-drone');
 var http    = require('http');
-var fs    = require('fs');
-var dry = false;
+var fs      = require('fs');
+
+// simulation (dry = true) or actual flying (dry = false)
+var dry     = false;
 
 var client = arDrone.createClient();
 if (!dry) client.takeoff();
@@ -16,11 +24,14 @@ var rawStream = arDrone.createRawStream({frameRate:5});
 rawStream
 	.on('error', console.log)
 	.on('data', function(rawBuffer) {
+	
+		// When you receive a new uncommpressed frame, analyse it and find the marker
 		var p = scanBuffer(rawBuffer);
+		
 		var size = Math.max(p.w, p.h);
 		
-		//client.stop();
-		//console.log(point);
+		// use the position of the marker to calculate commands for the drone
+		// here is a lot of potential for optimization!
 		if (size > 1) {
 			var msg = [p.w, p.h];
 		
@@ -31,11 +42,9 @@ rawStream
 			
 			if (d < 0) {
 				client.counterClockwise(a/reduceSpeed);
-				//client.left(a);
 				msg.push(repeat('L', a, 10));
 			} else {
 				client.clockwise(a/reduceSpeed);
-				//client.right(a);
 				msg.push(repeat('R', a, 10));
 			}
 		
@@ -72,34 +81,42 @@ rawStream
 		}
 	});
 	
+
 function scanBuffer(buf) {
 	var offset = 64;
 
 	var i = 0;
-	var cS = 0;
-	var xS = 0;
-	var yS = 0;
-	var xxS = 0;
-	var yyS = 0;
 	
-	//fs.writeFileSync('test.raw', buf);
+	// We need the following variables to calculate the center and the size of the marker
+	var cS = 0;  // sum of c (c = color value)
+	var xS = 0;  // sum of c*x
+	var yS = 0;  // sum of c*y
+	var xxS = 0; // sum of c*x*x
+	var yyS = 0; // sum of c*y*y
+	
 	
 	for (var y = 0; y < 360; y++) {
 		for (var x = 0; x < 640; x++) {
+		
+			// get the r,g,b values of every pixel
 			var r = buf[i+0];
 			var g = buf[i+1];
 			var b = buf[i+2];
+			
+			// calculate only the weight of green colors
 			var c = g - Math.max(r,b);
 			c = c - offset;
 			
 			if (c < 0) c = 0;
 			
+			// sum up the values
 			cS  += c;
 			xS  += x*c;
 			xxS += x*x*c;
 			yS  += y*c;
 			yyS += y*y*c;
 			
+			// for debugging
 			c = Math.round(255*c/(255-offset));
 			buf[i+0] = c;
 			buf[i+1] = c;
@@ -109,6 +126,7 @@ function scanBuffer(buf) {
 		}
 	}
 	
+	// generate some ascii art for debugging
 	var yMax = 18;
 	var xMax = 64;
 	var w = 640/xMax;
@@ -126,7 +144,6 @@ function scanBuffer(buf) {
 					c++;
 				}
 			}
-			//console.log(s);
 			s = Math.round(20*s/(255*c));
 			if (s > 5) s = 5;
 			m += " .-+O#".charAt(s);
@@ -134,10 +151,7 @@ function scanBuffer(buf) {
 		console.log(m);
 	}
 	
-	//fs.writeFileSync('post.raw', buf);
-	
-	//xSum /= cSum;
-	//ySum /= cSum;
+	// return the center, the size and the quality/clarity of the marker
 	return {
 		x:xS/cS,
 		y:yS/cS,
@@ -153,21 +167,3 @@ function repeat(c, value, max) {
 	for (var i = 0; i < max; i++) s += (value*max > i) ? c : '.';
 	return s;
 }
-	
-	
-/*
-var server = http.createServer(function(req, res) {
-  if (!lastPng) {
-    res.writeHead(503);
-    res.end('Did not receive any raw data yet.');
-    return;
-  }
-
-  res.writeHead(200, {'Content-Type': 'image/raw'});
-  res.end(lastPng);
-});
-
-server.listen(8080, function() {
-  console.log('Serving latest raw on port 8080 ...');
-});
-*/
